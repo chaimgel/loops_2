3ed148a34b8015b5870c32d5322885195cb41ee7
chaimgel <ycgel770@gmail.com>
2025-06-19 14:38:36 +0300

    adding exercise 1

---
+++ answers.py
@@ -0,0 +1,10 @@
+# exercise 1
+for i in range(1,1001):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FuzzBuzz")
+    elif i%3==0:
+        print("Fuzz")
+    elif i%5==0:
+        print("Buzz")
+    else:
+        print(i)
